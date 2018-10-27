bd465cb8db899448e61b60a98515ed144fa8d7f8
Belle <bellexchuang@gmail.com>
2018-10-27 12:43:01 -0700

    adding all project pages

--- code.js
+++ code.js
@@ -5,13 +5,6 @@ $("#sendEmail").click(function() {
   var email = $("#email").val();
   var message = $("#message").val();
 
-  //set HTML of .modal-body
-  $(".modal-body").html("Thank you, " + firstName + "! Your message has been sent!");
-  //Thank you, First Name. Your message has been sent!
-
-  //clear all fields in form
-  $(".form-control").val("");
-
   //if user enters first name, email, and message
     //show message has been sent 
   //if any input is missing
@@ -20,11 +13,38 @@ $("#sendEmail").click(function() {
     if (firstName && email && message) {
 
       //set HTML of .modal-body
-      $(".modal-body").html("Thank you, " + firstName + "! Your message has been sent!");
+      $(".modal-body").html("Yay, " + firstName + "! Your message has been sent!");
+
+        //clear all fields in form
+      $(".form-control").val("");
       
     } else {
       //set HTML of .modal-body
-      $(".modal-body").html("Check your inputs!");
+      $(".modal-body").html("Please fill in all inputs");
     }
 
+});
+
+$("#contactbutton").click(function() {
+  $(this).find('i').toggleClass('fas fa-times far fa-envelope')
+});
+
+$(document).ready(function($){
+	var $timeline_block = $('.cd-timeline-block');
+
+	//hide timeline blocks which are outside the viewport
+	$timeline_block.each(function(){
+		if($(this).offset().top > $(window).scrollTop()+$(window).height()*0.75) {
+			$(this).find('.cd-timeline-img, .cd-timeline-content').addClass('is-hidden');
+		}
+	});
+
+	//on scolling, show/animate timeline blocks when enter the viewport
+	$(window).on('scroll', function(){
+		$timeline_block.each(function(){
+			if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
+				$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
+			}
+		});
+	});
 });
